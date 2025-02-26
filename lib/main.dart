6f49b78959e1dc975a0892d4ef9d0b0593113770
wlstn5555 <wlstn56789@gmail.com>
2025-02-26 23:56:29 +0900

    색깔 바꾸고 인ㅌ터페이스 수정했긔

--- lib/main.dart
+++ lib/main.dart
@@ -18,56 +18,58 @@ class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: Text(
-          '메인 화면',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 26,
-          ),
-        ),
-        centerTitle: true,
-        leading: Padding(
-          padding: const EdgeInsets.only(left: 16.0),
-          child: Icon(Icons.arrow_back, color: Colors.white),
-        ),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 16.0),
-            child: Icon(Icons.settings, color: Colors.white),
-          ),
-        ],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.blue,
-                fixedSize: Size(344, 342),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(16),
-                ),
-              ),
-              onPressed: () {},
-              child: Text(
-                '챗봇',
-                style: TextStyle(
-                  fontSize: 62,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
+        appBar: AppBar(
+          backgroundColor: Color(0xFF547EE8),
+          //backgroundColor: Colors.red,
+          title: Text(
+            '메인 화면',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 26,
             ),
-            SizedBox(height: 20),
-            _buildButton('복용 알림/여부'),
-            _buildButton('주의사항 녹음'),
-            _buildButton('카메라 인식'),
+          ),
+          centerTitle: true,
+          leading: Padding(
+            padding: const EdgeInsets.only(left: 16.0),
+            child: Icon(Icons.arrow_back, color: Colors.white),
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 16.0),
+              child: Icon(Icons.settings, color: Colors.white),
+            ),
           ],
         ),
-      ),
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SizedBox(height: 10),  // 챗봇 버튼 위에 20px 여백 추가
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFF547EE8),
+                  fixedSize: Size(340, 342),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                ),
+                onPressed: () {},
+                child: Text(
+                  '챗봇',
+                  style: TextStyle(
+                    fontSize: 62,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              _buildButton('복용 알림/여부'),
+              _buildButton('주의사항 녹음'),
+              _buildButton('카메라 인식'),
+            ],
+          ),
+        )
     );
   }
 
@@ -76,7 +78,7 @@ class MainScreen extends StatelessWidget {
       padding: const EdgeInsets.only(bottom: 10.0),
       child: ElevatedButton(
         style: ElevatedButton.styleFrom(
-          backgroundColor: Colors.blue,
+          backgroundColor: Color(0xFF547EE8),
           fixedSize: Size(344, 86),
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(16),
